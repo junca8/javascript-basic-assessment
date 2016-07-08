445349f0e1e13e0d961ebc304acea19aab897d6c
Jonathan Junca <Jonathan@Jonathans-MacBook-Pro.local>
2016-07-08 14:34:54 -0600

    test

--- assessment.js
+++ assessment.js
@@ -1,9 +1,11 @@
 //#1 Create a var called hello that says goodbye
 
-
+var hello = "goodbye";
 
 //#2 Use iLove to create a new var called iLoveCode that says "I love code"
 var iLove = "I love"
+
+var iLoveCode = iLove + " code";
 
 //#3  Make an object called bob
 // bob has a height of 6ft (string)
@@ -12,6 +14,16 @@ var iLove = "I love"
 // bob is not presidentOfTheUnitedStates (boolean)
 // bob likes apples, bananas, and cherries (array of strings)
 
+var bob = {
+  height: "6ft",
+  age: 24,
+  hair: {
+    style: "spikey",
+    color: "brown"
+  },
+  presidentOfTheUnitedStates: false,
+  likes: ["apples", "bananas", "cherries"]
+};
 
 //#4 Change my shirt color to pink using dot notation
 var myShirt = {
@@ -19,17 +31,25 @@ var myShirt = {
   color: "red"
 }
 
+myShirt.color = "pink";
+
 // Change my shirt type to spandex using square bracket notation
 var myOtherShirt = {
   type: "polo",
   color: "red"
 }
 
+myOtherShirt["type"] = "spandex";
+
 //#5 Create an object that tracks a count of animals in a zoo.  Call it 'zoo'
 // The key should be the animal name(string) and the value should be how many there are.
 // Our zoo has 8 monkeys, 4 giraffe’s and 2 elephants
 
-
+var zoo = {
+  monkeys: 8,
+  giraffes: 4,
+  elephants: 2
+}
 
 //#6 Loop through this object and change all properties that start with the letter s to have a value of 's'
 var snake = {
@@ -40,54 +60,132 @@ var snake = {
   runs: 'legless'
 }
 
+for (var prop in snake) {
+  if (prop.charAt(0) === "s") {
+    snake[prop] = "s";
+  }
+}
+
 
 //#7 Create an array of strings that are the 6 primary colors (lower-case). Call your array primaryColors
+
+var primaryColors = ["yellow", "red", "blue", "green", "orange", "violet", "indigo"];
 
 //#8 Using this array do the following
 var heroes = ['superman', 'batman', 'flash'];
 //add 'wonderwoman' to the end
-
+heroes.push("wonderwoman");
 //remove 'superman' and store him in a var called firstHero
-
+var firstHero = heroes.shift();
 //add 'spongebob' to the start of the array
-
+heroes.unshift("spongebob");
 //remove 'flash' from the array and store him in a var called secondHero
-
+var secondHero = heroes.splice(2, 1)[0];
 //leave batman in the array but put a copy of him on a var called thirdHero
+var thirdHero = heroes[1];
 
 
- 
 
 //#9 Write a function called addItem, that takes in an array, and adds the item to
 // the array.
- 
+
+var addItem = function(array, item) {
+  array.push(item);
+  return array;
+}
 
 //#10 Write a function called removeItem, that takes in an array of strings, and a string.
 // Removes all instances of that string from the array. And return the modified array.
 // The order of the array should not be changed
 
+var removeItem = function(array, str) {
+  for (var i = 0; i < array.length; i++) {
+    if (array[i] === str) {
+      array.splice(i, 1);
+      i--;
+    }
+  }
+  return array;
+}
+
 
 //#11 Write a function doubleTheFun that takes 1 parameter it doubles numbers, and
 // repeats strings example 4->8, 2.5->5, 'Awesome'->'AwesomeAwesome'
-
+var doubleTheFun = function(par) {
+  if(typeof par === "string" && isNaN(parseInt(par))) {
+    par = par + par;
+  }
+  else if (isNaN(parseInt(par))) {
+    par = parseInt(par) * 2;
+  }
+  else {
+    par = par * 2;
+  }
+  return par;
+}
 
 //#12 Write function getValueOfProperty that takes in an object, and the name of a property on the object
 // return the value from the object that corresponds to the property
+var getValueOfProperty = function(obj, name) {
+  return obj[name];
+}
 
 //#13 Write a function called makeChatMessage that takes in a message and author parameter
 // and returns an object with a message, author, and timestamp, that is
 // the current time as a Date object
 
-//#14 Create a function called coderTest that takes in an object that is a person, it looks to see if the person’s name is Jeremy and changes the person object to have a property called lovesCode with a value of 10.  If their name is Brack set lovesCode to 0.  otherwise set lovesCode to 5. 
+var makeChatMessage = function(message, author) {
+  var timestamp = new Date();
+  return {
+    message: message,
+    author: author,
+    timestamp: timestamp
+  }
+}
 
+//#14 Create a function called coderTest that takes in an object that is a person, it looks to see if the person’s name is Jeremy and changes the person object to have a property called lovesCode with a value of 10.  If their name is Brack set lovesCode to 0.  otherwise set lovesCode to 5.
+var coderTest = function(person) {
+  if (person.name === "Jeremy") {
+    person.lovesCode = 10;
+  }
+  else if (person.name === "Brack") {
+    person.lovesCode = 0;
+  }
+  else {
+    person.lovesCode = 5;
+  }
+  return person;
+}
 //#15 Create a function called outside that takes in a temperature (number), a humidity(number), and a cloudiness(number), in that order. Using the following to return the correct values
 /*
     temperature over 80 and humidity over 40 - return "I'm all sweat"
     temperature under 40 and cloudiness over 60 - return "I have icecicles"
     temperature over 80 and humidity under 40 and cloudiness under 20 - return "I'm literally in the desert"
     temperature over 80 or humidity over 50 or cloudiness over 50 - return "Hmm, probably not"
-    Otherwise - return "I love outside" 
+    Otherwise - return "I love outside"
 */
+var outside = function(temp, humid, cloud) {
+  if (temp > 80 && humid > 40) {
+    return "I'm all sweat";
+  }
+  else if (temp < 40 && cloud > 60) {
+    return "I have icecicles";
+  }
+  else if (temp > 80 && humid < 40 && cloud < 20) {
+    return "I'm literally in the desert";
+  }
+  else if (temp > 80 || humid > 50 || cloud > 50) {
+    return "Hmm, probably not";
+  }
+  else {
+    return "I love outside";
+  }
+}
+
 
 //#16 Create a function called callerBack that takes in a function (holla) and a string parameter(back) and invokes it(holla) with the string(back) + ' back'.
 //IE - If I call you with 'Give it' you should invoke holla with 'Give it back'
+var callerBack = function(holla, back) {
+  var newstr = back + " back";
+  holla(newstr);
+}
